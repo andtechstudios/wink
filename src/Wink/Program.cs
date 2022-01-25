a26b68b4d0e360933c42741041d0b4bee12a0486
Andrew <andrewmjordan@users.noreply.github.com>
2022-01-25 09:40:20 -0800

    init

---
+++ src/Wink/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using WindowsInput;
+using WindowsInput.Native;
+
+public class Program
+{
+
+	public static void Main(string[] args)
+	{
+		VirtualKeyCode code;
+		switch (args[0])
+		{
+			case "play":
+				code = VirtualKeyCode.PLAY;
+				break;
+			case "pause":
+				code = VirtualKeyCode.PAUSE;
+				break;
+			case "playpause":
+				code = VirtualKeyCode.MEDIA_PLAY_PAUSE;
+				break;
+			case "previous":
+				code = VirtualKeyCode.MEDIA_PREV_TRACK;
+				break;
+			case "next":
+				code = VirtualKeyCode.MEDIA_NEXT_TRACK;
+				break;
+			case "stop":
+				code = VirtualKeyCode.MEDIA_STOP;
+				break;
+			case "mute":
+				code = VirtualKeyCode.VOLUME_MUTE;
+				break;
+			default:
+				throw new ArgumentException($"Unsupported key event: {args[0]}");
+		}
+
+		new InputSimulator()
+			.Keyboard
+			.KeyPress(code);
+	}
+}
+
